99cdf6bf88ba8acc3dad50994752a70c9f075831
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-09 03:00:08 +0300

    redux dispatch

--- src/index.js
+++ src/index.js
@@ -4,11 +4,48 @@ import AppRouter from './routers/AppRouter';
 import "./App.css";
 import { createStore } from 'redux';
 
-const store = createStore((state = {count: 0}) => {
-    return state
+const initialState = {
+    count: 0
+}
+
+const store = createStore((state = initialState, action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            const incrementBy = typeof action.incrementBy === "number" ? action.incrementBy: 1;
+            return {
+                count: state.count + incrementBy
+            }
+        case "DECREMENT":
+            return {
+                count: state.count-1
+            }
+        case "RESET":
+            return {
+                count: 0
+            }
+        default:
+            return state
+    }
 })
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch({
+    type: "INCREMENT",
+    incrementBy: 10
+})
+
+// store.dispatch({
+//     type: "DECREMENT"
+// })
+
+// store.dispatch({
+//     type: "RESET"
+// })
+
+// console.log(store.getState());
 
 const container = document.getElementById('app');
 const root = createRoot(container);
